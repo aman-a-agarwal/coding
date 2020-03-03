2b013e955c8afa4bb7e982431cd4d900c3e0b082
Aman Agarwal <aman.a.agg@gmail.com>
2020-03-03 19:40:50 +1030

    Commit: arrayRotation.py

---
+++ arrayRotation.py
@@ -0,0 +1,11 @@
+def rotLeft(a, d):
+    map_pos = [(i - d) for i in range(len(a))]
+    returnList = [None for i in range(len(a))]
+    for idx, ele in enumerate(map_pos):
+        returnList[ele] = a[idx]
+    return returnList
+
+arr = [1,2,3,4,5]
+d = 4
+
+r = rotLeft(arr, d)

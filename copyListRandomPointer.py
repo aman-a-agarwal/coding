58e2c1a6396b222d349b50a1ef59dbec600f6c9a
Aman Agarwal <aman.a.agg@gmail.com>
2020-03-03 20:10:01 +1030

    Revise: copyListRandomPointer.py

--- copyListRandomPointer.py
+++ copyListRandomPointer.py
@@ -9,30 +9,57 @@ null = None
 
 class Solution:
     def copyRandomList(self, head: Node) -> Node:
-        if (head == null):
-            return null;
+        if head is None:
+            return None
+
         dummy = Node(0)
         current = dummy
         nodeMap = dict()
 
-        while (head != null):
-            if (head not in nodeMap.keys()):
+        while (head is not None):
+            if (head.val not in nodeMap):
                 newNode = Node(head.val)
-                nodeMap[head] = newNode
             else:
-                newNode = nodeMap.get(head)
+                newNode = nodeMap.get(head.val)
 
-            if (head.random != null):
-                if (head.random not in nodeMap.keys()):
-                    newNode.random = Node(head.random.val)
-                    nodeMap[head.random] = newNode.random
-                else:
-                    newNode.random = nodeMap.get(head.random)
+
+            if head.random is None:
+                randomNode = None
+            elif head.random.val not in nodeMap:
+                randomNode = Node(head.random.val)
             else:
-                newNode.random = null
+                randomNode = nodeMap.get(head.random.val)
+
+            newNode.random = randomNode
+            nodeMap[newNode.val] = newNode
+            if randomNode is not None:
+                nodeMap[randomNode.val] = randomNode
 
             current.next = newNode
-            current = newNode
             head = head.next
+            current = current.next
 
         return dummy.next
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+a.random = c
+b.random = None
+c.random = e
+d.random = a
+e.random = d
+
+s = Solution()
+copyList = s.copyRandomList(a)
+
+print(copyList)
